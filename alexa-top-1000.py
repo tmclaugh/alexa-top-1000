4a91fe58f7a3383a193b7828c50a08004de26303
Tom McLaughlin <tom.mclaughlin@threatstack.com>
2017-05-29 00:42:34 -0400

    Now we have word couns print

--- alexa-top-1000.py
+++ alexa-top-1000.py
@@ -12,7 +12,15 @@ def main(number_sites=5):
     # Get sites ordered
     sorted_site_list = alexa_top_1000.sort_sites_by_words(site_list)
 
-    print(sorted_site_list)
+    rank = 1
+    total_word_count = 0
+    for site in sorted_site_list:
+        print('{}) {}'.format(rank, site))
+        total_word_count += site.word_count
+        rank += 1
+
+    average_word_count = total_word_count / len(sorted_site_list)
+    print('\n\nAverage Word count: {}'.format(average_word_count))
 
 
 if __name__ == '__main__':
